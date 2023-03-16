f184e08bbca43d322f74200ef072b310ae150327
SergioLattuada <cslattuada@gmail.com>
2023-03-16 11:19:53 -0300

    Delete funcion.js

--- js/funcion.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contactame').onclick = function () {
-    console.log ("capturamos el click");    
-    document.getElementById("contenedor_del_boton").style.display = 'none'; 
-    document.getElementById("datos_de_contacto").style.visibility = 'visible'; 
-}
